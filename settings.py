a75f4a1b8ad0e494dca240e68cf79ae49bf2426d
Mennaahmed6687@gmail.com <mennaahmed6687@gmail.com>
2026-04-05 15:09:29 +0200

    final fix database

--- settings.py
+++ settings.py
@@ -83,8 +83,9 @@ WSGI_APPLICATION = 'MrProject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+    
+        'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         conn_health_checks=True,
     )
